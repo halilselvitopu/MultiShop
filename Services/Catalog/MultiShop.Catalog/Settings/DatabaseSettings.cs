a2a3d031d5ef02b805353370a6c83374356697b9
halilselvitopu <halilselvitopu@gmail.com>
2025-03-22 14:15:12 +0300

    Catalog microservice, entities and mongodb settings

---
+++ Services/Catalog/MultiShop.Catalog/Settings/DatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Catalog.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string ProductDetailName { get; set; }
+        public string ProductImageName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+}
