a1fee9ad07a593c557949ccaf2f7efb04f1cf2e9
halilselvitopu <halilselvitopu@gmail.com>
2025-04-03 02:29:07 +0300

    Redis settings for basket microservice

---
+++ Services/Basket/MultiShop.Basket.WebApi/Controllers/BasketsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Basket.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+    }
+}
---
+++ Services/Basket/MultiShop.Basket.WebApi/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Basket.WebApi.Dtos
+{
+    public class BasketItemDto
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductAmount { get; set; }
+        public decimal Price { get; set; }
+    }
+}
