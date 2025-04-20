5671be16f45989d8247026c47495f825bc04c0d8
halilselvitopu <halilselvitopu@gmail.com>
2025-04-20 03:32:56 +0300

    Categories, feature, feature products, offer discount, special offer, vendor view component and footer layout

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/DefaultFeatureViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class DefaultFeatureViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
