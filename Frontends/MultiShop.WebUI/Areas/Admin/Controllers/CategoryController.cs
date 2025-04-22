f059d156f34f63998b7b56078617cf6c9b519af9
halilselvitopu <halilselvitopu@gmail.com>
2025-04-22 19:04:13 +0300

    Admin area added and some admin layout arrangements have been made

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            ViewBag.v0 = "Kategori İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";           
+            return View();
+        }
+    }
+}
