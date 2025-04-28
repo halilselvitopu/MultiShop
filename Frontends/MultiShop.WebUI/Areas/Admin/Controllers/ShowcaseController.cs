144f39b015cfee212add309e6c9ec52cc23cbd74
halilselvitopu <halilselvitopu@gmail.com>
2025-04-28 03:46:26 +0300

    Implementation of Showcase CRUD operations for the admin panel and display on the home page

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ShowcaseController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ShowcaseDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    [Area("Admin")]
+    [Route("Admin/Showcase")]
+    public class ShowcaseController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ShowcaseController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v0 = "Vitrin İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Vitrinler";
+            ViewBag.v3 = "Vitrin Listesi";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7183/api/Showcases");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultShowcaseDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [Route("CreateShowcase")]
+        [HttpGet]
+        public async Task<IActionResult> CreateShowcase()
+        {
+            ViewBag.v0 = "Vitrin İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Vitrinler";
+            ViewBag.v3 = "Vitrin Listesi";
+            return View();
+        }
+
+        [Route("CreateShowcase")]
+        [HttpPost]
+        public async Task<IActionResult> CreateShowcase(CreateShowcaseDto createShowcaseDto)
+        {
+            ViewBag.v0 = "Vitrin İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Vitrinler";
+            ViewBag.v3 = "Vitrin Listesi";
+
+            createShowcaseDto.Status = false;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createShowcaseDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7183/api/Showcases", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Showcase", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+
+        [Route("DeleteShowcase/{id}")]
+        public async Task<IActionResult> DeleteShowcase(string id)
+
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7183/api/Showcases/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+
+                return RedirectToAction("Index", "Showcase", new { area = "Admin" });
+
+            return View();
+
+        }
+
+        [Route("UpdateShowcase/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateShowcase(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7183/api/Showcases/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateShowcaseDto>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [Route("UpdateShowcase/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateShowcase(UpdateShowcaseDto updateShowcaseDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateShowcaseDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7183/api/Showcases/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Showcase", new { area = "Admin" });
+            }
+
+            return View();
+        }
+    }
+}
--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/DefaultCaraouselViewComponent.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/DefaultCaraouselViewComponent.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ShowcaseDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class DefaultCaraouselViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultCaraouselViewComponent(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7183/api/Showcases");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultShowcaseDto>>(jsonData);
+                ViewBag.SliderCount = values.Count;
+                return View(values);
+            }
+
             return View();
+
         }
     }
 }
